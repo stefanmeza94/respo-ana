98817ae35a47d8d932f807689029189bd63d5b4e
Stefan Meza <stefanmeza93@gmail.com>
2023-12-10 21:03:01 +0100

    Add hover on burger menu; Start to work on plus value func

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// Function 1
-
 // burger btn drop-down menu
 const burgerTablet = document.querySelector("#burger");
 const overlay = document.querySelector(".overlay");
 const mobileNav = document.querySelector(".mobile-nav");
+
+// subtract and add buttons
+const subtractBtn = document.getElementById("subtractBtn");
+const addBtn = document.getElementById("addBtn");
+let price = document.getElementById("price");
+let amountValue = document.getElementById("amount");
+
+addBtn.addEventListener("click", function (e) {
+  const mulitplyByAmount = Number(amountValue.textContent) + 1;
+  const tempPrice = Number(price.textContent);
+  let multiplicationResult = mulitplyByAmount * tempPrice;
+
+  amountValue.textContent = mulitplyByAmount;
+  price.textContent = multiplicationResult;
+});
+
+subtractBtn.addEventListener("click", function (e) {
+  console.log("subract");
+});
 
 // show menu on burger button click
 burgerTablet.addEventListener("click", toggleMenu);
@@ -18,36 +35,36 @@ function toggleMenu(e) {
 }
 
 // hide menu on body click
-document.querySelector("body").addEventListener("click", function () {
-  itemsMenu.classList.remove("open");
-});
-
-itemsMenu.addEventListener("click", function (e) {
-  e.stopPropagation();
+overlay.addEventListener("click", function () {
+  mobileNav.classList.remove("active");
+  overlay.classList.remove("active");
 });
 
 document.addEventListener("keydown", function (e) {
   if (e.code === "Escape") {
-    itemsMenu.classList.remove("open");
+    mobileNav.classList.remove("active");
+    overlay.classList.remove("active");
   }
 });
 
 // Function 2
 // add to cart btns
-const orangeBtn = document.querySelector("#orangeBtn");
-const subtractBtn = document.querySelector("#subtractBtn");
-const addBtn = document.querySelector("#addBtn");
-const priceTotal = document.querySelector("#price");
+// const orangeBtn = document.querySelector("#orangeBtn");
+// const subtractBtn = document.querySelector("#subtractBtn");
+// const addBtn = document.querySelector("#addBtn");
+// const priceTotal = document.querySelector("#price");
 
-const amount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let sum = 2999;
+// const amount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 2999;
 
-function addToCart(e) {
-  addBtn.addEventListener("click", function () {
-    for (let i = 1; i < amount.length; i++) {
-      sum *= amount[i];
+// function addToCart(e) {
+// addBtn.addEventListener("click", function (e) {
+//   for (let i = 1; i < amount.length; i++) {
+//     sum *= amount[i];
 
-      priceTotal.innerText = e.target.value;
-    }
-  });
-}
+//     priceTotal.innerText = e.target.value;
+//   }
+
+//   console.log(sum);
+// });
+// }
